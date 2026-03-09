a2fefe85297b8ecfcdb0ab8b43ee3e8a8f6304d6
jay-isampelliwar <isampelliwar79@gmail.com>
2026-03-10 00:25:48 +0530

    Refactoring

--- scripts/data_ingestion.py
+++ scripts/data_ingestion.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from src.chroma_db import ChromaDatabase
@@ -5,21 +6,21 @@ from src.document_ingestion import DocumentIngestion
 from src.embedding_manager import EmbeddingManager
 
 
+DEFAULT_EVAL_DOCS = "data/docs"
+CHROMA_COLLECTION=os.getenv("CHROMA_COLLECTION")
+
 def run() -> None:
     """Run the end-to-end data ingestion pipeline."""
 
     print("Running data ingestion pipeline...")
 
-    base_dir = Path(__file__).resolve().parents[1]
-    docs_dir = base_dir / "data" / "docs"
-
     embedding_manager = EmbeddingManager()
-    vector_store = ChromaDatabase(collection_name="research-papers")
+    vector_store = ChromaDatabase(collection_name=CHROMA_COLLECTION)
 
     data_ingestion = DocumentIngestion(
         vector_store=vector_store,
         embedding_manager=embedding_manager,
-        path_to_document=str(docs_dir),
+        path_to_document=str(DEFAULT_EVAL_DOCS),
     )
 
     data_ingestion.load_document()
